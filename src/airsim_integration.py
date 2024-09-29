b3adc4ac0c67857ebdee11932cf1fcd6ef98356e
Saswata Kumar Dash <94286342+saswatakumardash@users.noreply.github.com>
2024-09-29 21:17:53 +0530

    Update airsim_integration.py

--- src/airsim_integration.py
+++ src/airsim_integration.py
@@ -1,6 +1,6 @@
 import airsim
 import numpy as np
-import time
+from src.lstm_model import DQNLSTM
 from src.safety_checker import SafetyChecker
 
 class DroneMOAAPP:
@@ -16,6 +16,7 @@ class DroneMOAAPP:
         self.safety_checker = SafetyChecker(self.client, config)
 
     def get_state(self):
+        """Get the current state of the UAV (position, velocity)"""
         state = self.client.getMultirotorState()
         position = state.kinematics_estimated.position
         velocity = state.kinematics_estimated.linear_velocity
@@ -24,61 +25,51 @@ class DroneMOAAPP:
             velocity.x_val, velocity.y_val, velocity.z_val
         ])
 
-    def predict_next_waypoint(self, goal):
-        if len(self.state_history) < self.config.SEQ_LENGTH:
-            return goal  # If not enough history, move towards the goal
+    def predict_next_action(self, state):
+        """Predict the next action using the RL model"""
+        state = state.reshape((1, self.config.SEQ_LENGTH, 6))  # Reshape to fit model input
+        action = self.model.act(state)
+        return action
 
-        recent_states = np.array(self.state_history[-self.config.SEQ_LENGTH:])
-        prediction = self.model.predict(recent_states.reshape(1, self.config.SEQ_LENGTH, 6))
-        return prediction[0]
-
-    def move_drone(self, waypoint):
+    def move_drone(self, action):
+        """Move the drone based on the action"""
+        waypoint = self.config.ACTION_MAP[action]  # Map the action to a movement command
         if self.safety_checker.is_safe_move(waypoint):
             self.client.moveToPositionAsync(waypoint[0], waypoint[1], waypoint[2], 5).join()
         else:
             print("Unsafe move detected. Executing collision avoidance.")
             self.safety_checker.avoid_collision()
 
-    def is_waypoint_reached(self, current_position, waypoint):
-        distance = np.linalg.norm(current_position - waypoint)
-        return distance < self.config.DISTANCE_THRESHOLD
-
-    def takeoff(self, location):
-        print(f"Taking off at location: {location}")
-        self.client.takeoffAsync().join()
-        self.move_drone(location)
-
-    def land(self, location):
-        print(f"Landing at location: {location}")
-        self.move_drone(location)
-        self.client.landAsync().join()
-
     def run_mission(self, mission):
+        """Run the mission (start with takeoff, navigate to waypoints, land)"""
         self.takeoff(mission['takeoff'])
 
         for waypoint in mission['waypoints']:
             print(f"Moving to waypoint: {waypoint}")
-            self.navigate_to_waypoint(waypoint)
+            for step in range(self.config.MAX_STEPS):
+                current_state = self.get_state()
+                self.state_history.append(current_state)
+                if len(self.state_history) < self.config.SEQ_LENGTH:
+                    continue
+
+                # Get the next action from the model
+                state = np.array(self.state_history[-self.config.SEQ_LENGTH:])
+                action = self.predict_next_action(state)
+                self.move_drone(action)
 
         self.land(mission['landing'])
         print("Mission complete.")
 
-    def navigate_to_waypoint(self, goal):
-        for step in range(self.config.MAX_STEPS):
-            current_state = self.get_state()
-            self.state_history.append(current_state)
-            current_position = current_state[:3]
+    def takeoff(self, location):
+        """Takeoff at the specified location"""
+        self.client.takeoffAsync().join()
+        self.move_drone(location)
 
-            if self.is_waypoint_reached(current_position, goal):
-                print(f"Waypoint reached in {step} steps!")
-                break
-
-            next_waypoint = self.predict_next_waypoint(goal)
-            print(f"Moving to intermediate point: {next_waypoint}")
-            self.move_drone(next_waypoint)
-
-            time.sleep(self.config.STEP_DELAY)
+    def land(self, location):
+        """Land at the specified location"""
+        self.client.landAsync().join()
 
     def cleanup(self):
+        """Clean up the UAV after the mission"""
         self.client.armDisarm(False)
         self.client.enableApiControl(False)
